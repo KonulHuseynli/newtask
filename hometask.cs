2317aca264b0627b763ad4ba0a220c87c8bbf452
KonulHuseynli <konulrh@code.edu.az>
2022-05-18 23:26:38 +0400

    addedtask

---
+++ hometask.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConsoleApp39
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr1 = { 2, 3, 4, 7 };//2347
+            int[] arr2 = { 3,2};//34
+           
+            Console.WriteLine( IsArrayContains(arr1,arr2));
+        }
+        static bool IsArrayContains(int [] arr1,int [] arr2)
+        {
+            int counter = 0;
+            for (int i = 0; i <arr1.Length; i++)
+            {
+                for (int j = 0; j < arr2.Length; j++)
+                { 
+                    if(arr2[j]==arr1[i] &&  )
+                    {
+                        counter++;
+                        if(counter==arr2.Length)
+                        {
+                            return true;
+                        }
+                    }
+
+                }
+
+            }return false;
+        }
+    }
+}
+
